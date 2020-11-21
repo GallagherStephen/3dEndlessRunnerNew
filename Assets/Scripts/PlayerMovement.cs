22abd80f30ebb80f2d4585f629c1b8acfc6b98f2
GallagherStephen <37175825+GallagherStephen@users.noreply.github.com>
2020-11-21 18:25:11 +0000

    added jump to main player works perfect when gravity applied but player dies when touching plane

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,9 +5,13 @@ public class PlayerMovement : MonoBehaviour{
     public float moveSpeed;
     bool controlsActive;
 
+    public Rigidbody rb; //for jump
+    public bool playerIsOnTheGround = true; // for jump
+
     // Start is called before the first frame update
     private void Start()
     {
+        rb = GetComponent<Rigidbody>(); //for jump
 
         controlsActive = true; //at the start allow controls to be true
         moveSpeed = 25f; //movement speed
@@ -29,8 +33,22 @@ public class PlayerMovement : MonoBehaviour{
     private void Movement()
     {
         transform.Translate(moveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime, 0f, moveSpeed * Input.GetAxis("Vertical") * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && playerIsOnTheGround)
+        {
+            rb.AddForce(new Vector3(0, 5, 0), ForceMode.Impulse);
+            playerIsOnTheGround = false;
+        }
+            
     }
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.name == "floor")
+        {
+            playerIsOnTheGround = true;
+        }
+    }
 
     private void PlayerHasDied()
     {
