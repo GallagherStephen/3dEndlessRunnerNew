9b4e956df03b1b73c466a3b1ee3195f085e7a2c0
GallagherStephen <37175825+GallagherStephen@users.noreply.github.com>
2020-12-10 15:25:11 +0000

    added sounds,correct powerups to each level which destroy on enter,added particle effect to powerups.

---
+++ Assets/Scripts/ShieldPoweUp.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShieldPoweUp : MonoBehaviour
+{
+    public float multiplier = 1.4f;
+
+    public GameObject pickupEffect;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("player"))
+        {
+            Pickup();
+        }
+    }
+
+    void Pickup()
+    {
+        Instantiate(pickupEffect, transform.position, transform.rotation);
+
+        //player.transform.localScale *= multiplier;
+
+        Destroy(gameObject); //destroy shield
+    }
+
+
+
+
+}
---
+++ Assets/Scripts/VPNpowerUP.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VPNpowerUP : MonoBehaviour
+{
+
+    public float multiplier = 1.4f;
+
+    public GameObject pickupEffect;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("player"))
+        {
+            Pickup();
+        }
+    }
+
+    void Pickup()
+    {
+        Instantiate(pickupEffect, transform.position, transform.rotation);
+
+        //do powerup here:
+        //player.transform.localScale *= multiplier;
+
+        Destroy(gameObject); //destroy shield
+    }
+
+
+
+
+
+
+}
