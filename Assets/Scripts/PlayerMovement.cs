0dc942e63f28f75660e7f64eb471f2b7f99e7285
GallagherStephen <37175825+GallagherStephen@users.noreply.github.com>
2020-12-14 21:11:31 +0000

    making sure updated commit

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System.Collections;
 
 public class PlayerMovement : MonoBehaviour{
 
@@ -8,6 +9,10 @@ public class PlayerMovement : MonoBehaviour{
     public Rigidbody rb; //for jump
     public bool playerIsOnTheGround = true; // for jump
 
+    public float horizontalThrow;
+    public float yPos = -15;
+    
+
     // Start is called before the first frame update
     private void Start()
     {
@@ -15,6 +20,8 @@ public class PlayerMovement : MonoBehaviour{
 
         controlsActive = true; //at the start allow controls to be true
         //moveSpeed = 35f; //movement speed
+
+        
 
     }
 
@@ -32,7 +39,24 @@ public class PlayerMovement : MonoBehaviour{
 
     private void Movement()
     {
-        transform.Translate(moveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime, 0f,0f);
+        //transform.Translate(moveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime, 0f,0f);
+
+        //input
+        horizontalThrow = Input.GetAxis("Horizontal");
+        //speed
+        float xOffset = horizontalThrow / moveSpeed;
+        //clamp to screen width
+        float xPos = Mathf.Clamp(transform.localPosition.x + xOffset, -1.7f, 1.7f);
+        float yPos = Mathf.Clamp(transform.localPosition.y, 0, 10);
+        //update position
+        transform.position = new Vector3(xPos,yPos, transform.position.z);
+
+
+        //To clamp player range on x-axis
+        //transform.Translate(Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime, 0, 0);
+       // transform.position = new Vector3(Mathf.Clamp(transform.position.x, xLeftValueLimit, xRightValueLimit));
+
+
 
         if (Input.GetButtonDown("Jump") && playerIsOnTheGround)
         {
